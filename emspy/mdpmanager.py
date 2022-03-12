16153a3ed7de7b108aa8fd29934510511cf4bdd5
Chris (mechyai) <jpeubel.ai@gmail.com>
2022-03-12 17:11:13 -0500

    moved create mdp to this folder, udpated function

--- emspy/mdpmanager.py
+++ emspy/mdpmanager.py
@@ -1,3 +1,8 @@
+"""
+Classes aimed at helping managing all the EMS variables used during simulations to track data and control systems.
+MDP = Markov Decision Process, and reference to Reinforcement Learning
+"""
+
 from collections.abc import Sequence
 from typing import Callable
 
@@ -71,7 +76,10 @@ class MdpManager:
     EMS_types = ('var', 'intvar', 'meter', 'weather', 'actuator')
 
     def __init__(self):
-        """A manager class for all the created EMS element objects."""
+        """
+        A manager class for all the created EMS element objects. Each MDP element added creates a new MdpElement
+        instance and attribute referencing that MdpElement obj.
+        """
         # store EMS element variable names and handle IDs
         self.tc_var = {}
         self.tc_intvar = {}
@@ -80,7 +88,7 @@ class MdpManager:
         self.tc_actuator = {}
 
         self.ems_type_dict = {'var': [], 'intvar': [], 'meter': [], 'weather': [], 'actuator': []}
-        self.ems_master_list = {}
+        self.ems_master_list = {}  # stores list of ALL MdpElemnts used
 
     def add_ems_element(self,
                         ems_type: str,
@@ -117,18 +125,20 @@ class MdpManager:
         self.ems_type_dict[ems_type].append(ems_obj)
         self.ems_master_list[ems_element_name] = ems_obj
 
-    def get_ems_names(self, ems_objects: [MdpElement]):
+    def get_ems_names(self, ems_objects: [MdpElement] = None) -> list:
         """From list of MdpElement objects, their names are returned in order."""
+
+        if None:
+            # All MdpElements
+            ems_objects = self.ems_master_list.values()
+
         names_list = []
         for ems_obj in ems_objects:
             names_list.append(ems_obj.name)
 
-        if len(names_list) == 1:
-            return names_list[0]
-        else:
-            return names_list
+        return names_list
 
-    def update_ems_value(self, ems_objects: [MdpElement], ems_values: Sequence):
+    def update_ems_value(self, ems_objects: [MdpElement], ems_values: Sequence) -> dict:
         """
         Takes list of EMS objects and their values, stores all data, and returns encoded values in order of name list.
 
@@ -140,7 +150,7 @@ class MdpManager:
             raise (f'EMS element names {ems_objects} and their values {ems_values} are out of sync. They '
                    f'do not consist of the same number of values. They must align, element-wise.')
 
-        encoded_vals = {}
+        encoded_values = {}
         for i, ems_obj in enumerate(ems_objects):
             # update current value
             val = ems_values[i]
@@ -149,11 +159,12 @@ class MdpManager:
                 # if encoding function exists
                 val = self.run_encoding_fxn(ems_obj, val)
             # return encoded val, or if not encoding return normal val
-            encoded_vals[ems_obj.name] = val  # using ordered dict
+            encoded_values[ems_obj.name] = val  # using ordered dict
 
-        return encoded_vals
+        return encoded_values
 
-    def run_encoding_fxn(self, ems_object: MdpElement, value: float = None):
+    @staticmethod
+    def run_encoding_fxn(ems_object: MdpElement, value: float = None):
         """This carefully runs the encoding function for a given EMS obj and value, returning the encoded value."""
 
         # update encoded value
@@ -173,7 +184,7 @@ class MdpManager:
         ems_object.encoded_value = encoded_value
         return encoded_value
 
-    def get_obj_from_name(self, ems_name: str) -> MdpElement:
+    def get_mdp_element_from_name(self, ems_name: str) -> MdpElement:
         """Looks up and returns EMS object instance from its name."""
         return self.ems_master_list[ems_name]
 
@@ -183,7 +194,7 @@ class MdpManager:
         values_dict = {}
         for ems_obj in ems_objects:
             if type(ems_obj) == str:
-                ems_obj = self.get_obj_from_name(ems_obj)
+                ems_obj = self.get_mdp_element_from_name(ems_obj)
             values_dict[ems_obj.name] = ems_obj.value
 
         return values_dict
@@ -206,3 +217,72 @@ class MdpManager:
             values_dict[ems_obj.name] = encoded_value
 
         return values_dict
+
+    @staticmethod
+    def generate_mdp_from_tc(tc_intvars: dict = None,
+                             tc_vars: dict = None,
+                             tc_meters: dict = None,
+                             tc_weather: dict = None,
+                             tc_actuators: dict = None):
+        """
+        Given Table of Contents (TC) for each EMS-type, we create a MdpManager instance to more easily handle and store
+        the simulation variables generated at runtime.
+        This form-factor also helps implement automatic on normalizations on values for input to function approximators.
+
+        The dictionary for each EMS variable type [intvars: internal variables, vars: variables, etc.] must follow a
+        specific form:
+
+        = {
+            'ems_var1_name': [(...EMS handle ID parameters...)],  # no optional fxn and arguments
+            'ems_var2_name': [(...EMS handle ID parameters...), optional_fxn, fxn_arg_2,..., fxn_arg_n],
+            ...
+        }
+
+        Where each dictionary key is the user-defined name of that EMS element, and the value is a list of required
+        parameters (its 'handle' ID values) to find the EMS element in the E+ simulation & (optional) attached
+        normalization functions and its arguments. (Everytime the value of an EMS element is updated from the
+        simulation, so will its encoded value. This is used to simplify the normalization process of input variables
+        when working with Neural Networks, etc.)
+
+        Ex:
+        tc_vars = {
+            'hvac_operation_sched': [('Schedule Value', 'OfficeSmall HVACOperationSchd')],
+            'zn0_temp': [('Zone Air Temperature', zn0), normalize_min_max_saturate, 60, 80]
+        }
+
+        Looking at 'hvac_operation_sched': (a Schedule Value - EMS Variable)
+
+            The 1st element of the list value, the tuple, contains all the parameters specific to that EMS elements
+            'handle' ID. This is specific to your building model .IDF and requires EnergyPlus understanding to setup.
+            There is no encoding/normalization function included.
+
+        Looking at 'zn0_tmp': (a Zone temperature value - EMS Variable)
+
+            We see this variable includes an encoding/normalization function. Next to it are the 2nd and 3rd agrument
+            values to be called everytime this EMS element is updated, so that its encoded/normalized value will also
+            be updated.
+
+            These are the 2nd and 3rd arguments because for inorder for this user-written function
+            'normalize_min_max_saturate' to work, the first argument must be the EMS values itself.
+
+            EX:
+            def normalize_min_max_saturate(value: float, min: float, max: float):
+                lower = -1
+                upper = 1
+                if value > max:
+                    return upper
+                elif value < min:
+                    return lower
+                else:
+                    return ((value - min) / (max - min))*(upper - lower) + lower
+        """
+        mdp_instance = MdpManager()
+
+        # compile all ToCs and add to MDP class
+        master_toc = {'intvar': tc_intvars, 'var': tc_vars, 'meter': tc_meters, 'weather': tc_weather,
+                      'actuator': tc_actuators}
+        for ems_type, ems_tc in master_toc.items():
+            for ems_name, tc_values in ems_tc.items():
+                mdp_instance.add_ems_element(ems_type, ems_name, *tc_values[0:])
+
+        return mdp_instance
